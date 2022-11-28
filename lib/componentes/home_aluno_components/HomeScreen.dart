d66346fc3ee77caedf5077ee3508d5feaf49cb85
Matheus-Uchoa/ <matheusuchoa@unitins.br>
2022-11-28 10:40:50 -0300

    Adicionando widget dinâmico para Home

---
+++ lib/componentes/home_aluno_components/HomeScreen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:meuapp/componentes/home_aluno_components/menu_inferior_aluno.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  @override
+  MenuInferior createState() => MenuInferior();
+}
