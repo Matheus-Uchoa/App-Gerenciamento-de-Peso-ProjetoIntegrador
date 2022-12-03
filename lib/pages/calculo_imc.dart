87910235d2a4658b60fbb1a7df58339b34b2691c
Matheus-Uchoa/ <matheusuchoa@unitins.br>
2022-12-03 11:42:20 -0300

    Corrigindo erros na tela de calculo de imc

--- lib/pages/calculo_imc.dart
+++ lib/pages/calculo_imc.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 
 import 'TelaResultadoImc.dart';
+
 TextEditingController controllerPeso = TextEditingController();
 TextEditingController controllerAltura = TextEditingController();
 
@@ -17,162 +18,172 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.white,elevation: 0,),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
       body: SingleChildScrollView(
         reverse: true,
         child: Padding(
-            padding: EdgeInsets.all(40),
+            padding: EdgeInsets.all(20),
             child: Center(
                 child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: <Widget>[
-                      Text(
-                        'Calcular IMC',
-                        style: TextStyle(
-                            color: Colors.orange,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30.0,
-                            height: 3),
-                        textAlign: TextAlign.center,
-                      ),
-                      Text('Calcule para descrobir se você está no seu peso ideal',
-                          style: TextStyle(
-                              color: Color.fromARGB(255, 0, 0, 0),
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20),
-                          textAlign: TextAlign.center),
-                      Text('Gênero',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              height: 2),
-                          textAlign: TextAlign.left),
-                      Row(children: [
+                  Text(
+                    'Calcular IMC',
+                    style: TextStyle(
+                        color: Colors.orange,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0,
+                        height: 3),
+                    textAlign: TextAlign.center,
+                  ),
+                  Text('Calcule para descrobir se você está no seu peso ideal',
+                      style: TextStyle(
+                          color: Color.fromARGB(255, 0, 0, 0),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                      textAlign: TextAlign.center),
+                  Text('      Gênero',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          height: 2),
+                      textAlign: TextAlign.left),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
                         GestureDetector(
                             onTap: () {
                               sexo = homem;
                             },
-                            child: Image.asset(
-                              'assets/image/homi.png',
-                              height: 150,
-                              alignment: Alignment.centerLeft,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                'assets/homi.png',
+                                height: 150,
+                                alignment: Alignment.centerLeft,
+                              ),
                             )),
-                        SizedBox(width: 20, height: 10),
+                        // SizedBox(width: 60, height: 10),
                         GestureDetector(
                             onTap: () {
                               sexo = mulher;
                             },
-                            child: Image.asset(
-                              'assets/image/muie.png',
-                              height: 150,
-                              alignment: Alignment.centerLeft,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20),
+                              child: Image.asset(
+                                'assets/muie.png',
+                                height: 150,
+                                alignment: Alignment.centerLeft,
+                              ),
                             ))
                       ]),
-                      Row(
-                        children: [
-                          SizedBox(width: 25, height: 10),
-                          Text('Homem',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold,
-                                  height: 2)),
-                          SizedBox(width: 80, height: 10),
-                          Text(
-                            'Mulher',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
-                                height: 2),
-                            textDirection: TextDirection.rtl,
-                          ),
-                        ],
-                      ),
-                      Text('Dados',
+                  Row(
+                    children: [
+                      SizedBox(width: 55, height: 10),
+                      Text('Homem',
                           style: TextStyle(
                               color: Colors.black,
                               fontSize: 20,
                               fontWeight: FontWeight.bold,
                               height: 2)),
-                      SizedBox(height: 10),
-                      Row(
-                        children: [
-                          Flexible(
-                            child: Material(
-                                elevation: 5,
-                                borderRadius: BorderRadius.circular(20),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white54,
-                                  ),
-                                  child: TextFormField(
-                                      keyboardType: TextInputType.number,
-                                      controller: controllerPeso,
-                                      decoration: InputDecoration(
-                                          labelText: 'Peso',
-                                          hintText: 'Digite seu peso',
-                                          border: OutlineInputBorder(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(16.0))))),
-                                )),
-                          ),
-                          SizedBox(width: 10),
-                          Flexible(
-                            child: Material(
-                                elevation: 5,
-                                borderRadius: BorderRadius.circular(20),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.white54,
-                                  ),
-                                  child: TextFormField(
-                                      keyboardType: TextInputType.number,
-                                      controller: controllerAltura,
-                                      decoration: InputDecoration(
-                                          labelText: 'Altura',
-                                          hintText: 'Digite sua altura',
-                                          border: OutlineInputBorder(
-                                              borderRadius: BorderRadius.all(
-                                                  Radius.circular(16.0))))),
-                                )),
-                          ),
-                        ],
+                       SizedBox(width: 100, height: 10),
+                      Text(
+                        'Mulher',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            height: 2),
+                        textDirection: TextDirection.rtl,
                       ),
-                      Container(
-                        //margin: (EdgeInsets.only(top: 400)),
-                          margin: EdgeInsets.only(top: 119),
-                          padding: EdgeInsets.symmetric(horizontal: 90),
-                          decoration: BoxDecoration(
-                            color: Color.fromRGBO(242, 160, 7, 1),
-                            borderRadius: BorderRadius.all(Radius.circular(50)),
-                          ),
-                          child: TextButton(
-                              child: Center(
-                                  child: Text(
-                                    "CALCULAR",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  )),
-                              onPressed: () {
-
-                                Navigator.of(context).push(MaterialPageRoute(
-                                  builder: (context) => TelaResultadoImc(),
-                                ));
-                              }))
-                    ]))),
+                    ],
+                  ),
+                  Text('Dados',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          height: 2)),
+                  SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Material(
+                            elevation: 5,
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white54,
+                              ),
+                              child: TextFormField(
+                                  keyboardType: TextInputType.number,
+                                  controller: controllerPeso,
+                                  decoration: InputDecoration(
+                                      labelText: 'Peso',
+                                      hintText: 'Digite seu peso',
+                                      border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(16.0))))),
+                            )),
+                      ),
+                      SizedBox(width: 10),
+                      Flexible(
+                        child: Material(
+                            elevation: 5,
+                            borderRadius: BorderRadius.circular(20),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white54,
+                              ),
+                              child: TextFormField(
+                                  keyboardType: TextInputType.number,
+                                  controller: controllerAltura,
+                                  decoration: InputDecoration(
+                                      labelText: 'Altura',
+                                      hintText: 'Digite sua altura',
+                                      border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.all(
+                                              Radius.circular(16.0))))),
+                            )),
+                      ),
+                    ],
+                  ),
+                  Container(
+                      //margin: (EdgeInsets.only(top: 400)),
+                      margin: EdgeInsets.only(top: 119),
+                      padding: EdgeInsets.symmetric(horizontal: 90),
+                      decoration: BoxDecoration(
+                        color: Color.fromRGBO(242, 160, 7, 1),
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                      child: TextButton(
+                          child: Center(
+                              child: Text(
+                            "CALCULAR",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => TelaResultadoImc(),
+                            ));
+                          }))
+                ]))),
       ),
     );
   }
 }
 
-
 const selecaodosexo = Color.fromARGB(244, 140, 131, 131);
 String homem = 'homem';
 String mulher = 'mulher';
 String sexo = '';
-
-
